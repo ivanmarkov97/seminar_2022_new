22348be593018f2a9c426c06d8a27ce3396e338e
Ivan Markov <ivanmarkov1997@gmail.com>
2022-09-12 09:55:35 +0300

    refactor (#10)
    * refactor
    
    * refactor menu and css

--- sem2/db_work.py
+++ sem2/db_work.py
@@ -1,13 +1,23 @@
-from DBcm import UseDatabase
+from typing import Tuple, List
+from db_connection import DBConnection
 
 
-def work_with_db(dbconfig: dict, _sql: str):
-    with UseDatabase(dbconfig) as cursor:
+def work_with_db(db_config: dict, sql: str) -> Tuple[Tuple, List[str]]:
+    """
+    Выполняет запрос (SELECT) к БД с указанным конфигом и запросом.
 
+    Args:
+        db_config: dict - Конфиг для подключения к БД.
+        sql: str - SQL-запрос.
+    Return:
+        Кортеж с результатом запроса и описанеим колонок запроса.
+    """
+    result = tuple()
+    schema = []
+    with DBConnection(db_config) as cursor:
         if cursor is None:
-            raise ValueError('Курсор не создан')
-
-        cursor.execute(_sql)
+            raise ValueError('Cursor not found')
+        cursor.execute(sql)
         schema = [column[0] for column in cursor.description]
         result = cursor.fetchall()
     return result, schema
