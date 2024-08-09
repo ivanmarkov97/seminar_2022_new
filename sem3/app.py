46189a601b79c02d53daeb59d13f40b59a5c1d36
ivan <ivanmarkov1997@gmail.com>
2024-08-09 19:30:51 +0300

    renamed folder

--- sem3/app.py
+++ sem3/app.py
@@ -9,7 +9,7 @@ app: Flask = Flask(__name__)
 app.register_blueprint(blueprint_query, url_prefix='/requests')
 app.register_blueprint(blueprint_report, url_prefix='/reports')
 
-app.config['dbconfig'] = json.load(open('data_files/dbconfig.json'))
+app.config['dbconfig'] = json.load(open('configs/dbconfig.json'))
 
 
 @app.route('/', methods=['GET', 'POST'])
