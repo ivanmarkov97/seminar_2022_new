6c92cba0ae969b10a3144ee9a047ed76ec44c479
ivanmarkov1997 <ivanmarkov1997@gmail.com>
2024-09-18 12:36:04 +0300

    refactored viewer code

--- sem3/query/product_display.py
+++ sem3/query/product_display.py
@@ -10,14 +10,18 @@ class ProductInfo:
     data: tuple | None
     schema: list | None
     error_message: str
+    status: bool
 
 
 def get_product(db_config, user_input_data, sql_provider):
     error_message = ''
     if 'product_name' not in user_input_data:
         error_message = '@product_name not found'
-        return ProductInfo(data=None, schema=None, error_message=error_message)
+        return ProductInfo(data=None, schema=None, error_message=error_message, status=False)
 
     sql_query = sql_provider.get('product.sql', dict(input_product=user_input_data['product_name']))
+    print(sql_query)
     data, schema = select(db_config, sql_query)
-    return ProductInfo(data=data, schema=schema, error_message=error_message)
+    print(data)
+    print(schema)
+    return ProductInfo(data=data, schema=schema, error_message=error_message, status=True)
